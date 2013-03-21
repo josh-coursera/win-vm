06c2b71193ac5d73acfde2e616ba82cac13670d4
Joshua Newman <josh@coursera.org>
2013-03-20 21:31:37 -0700

    remove &&

--- setup.bat
+++ setup.bat
@@ -1,4 +1,4 @@
 netsh interface ip set address name="Local Area Connection" static 192.168.56.111 255.255.255.0 192.168.0.1 1
-&& echo 192.168.56.101 site.dev-coursera.org >>%SystemRoot%\system32\drivers\etc\hosts
-&& echo 192.168.56.101 spark.dev-coursera.org >>%SystemRoot%\system32\drivers\etc\hosts
+echo 192.168.56.101 site.dev-coursera.org >>%SystemRoot%\system32\drivers\etc\hosts
+echo 192.168.56.101 spark.dev-coursera.org >>%SystemRoot%\system32\drivers\etc\hosts
 
